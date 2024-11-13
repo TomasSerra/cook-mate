1e46bd83cf9d4820149d256a6e69bde8a8e94aff
TomasSerra <tomi.serra@gmail.com>
2024-11-13 15:28:12 -0300

    Image carrousel fixed storybook

--- src/components/common/imageCarrousel/imageCarrousel.stories.tsx
+++ src/components/common/imageCarrousel/imageCarrousel.stories.tsx
@@ -1,28 +1,36 @@
-import { Meta, StoryFn } from '@storybook/react';
-import ImageCarrousel, { ImageCarrouselProps } from './ImageCarrousel';
+import ImageCarrousel from './ImageCarrousel';
+import { Meta, StoryObj } from '@storybook/react';
 
+const meta: Meta<typeof ImageCarrousel> = {
+    title: 'UI/Header/ImageCarrousel',
+    component: ImageCarrousel,
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        images: {
+          control: 'text'
+        }
+    },
+};
+export default meta;
 
-const Template: StoryFn<ImageCarrouselProps> = (args) => (
-  <div style={{ width: '390px', height: '184px' }}>
-    <ImageCarrousel {...args} />
-  </div>
-);
+type Story = StoryObj<typeof ImageCarrousel>;
 
-export const Default = Template.bind({});
-Default.args = {
-  images: [
-    "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-    "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-  ],
+export const ImageCarrouselDefault: Story = {
+    args: {
+      images: [
+        "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+        "https://th.bing.com/th/id/OIP.74L1tQJeO5RJeCqyLFyWPAHaFb?w=246&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+      ],
+    },
+    render: (args) => (
+      <div style={{ width: '500px' }}>
+          <ImageCarrousel {...args} />
+      </div>
+  ),
 };
 
-
-export default {
-  title: 'UI/Header/ImageCarrousel',
-  component: ImageCarrousel,
-  parameters: {
-    layout: 'centered',
-  }
-} as Meta;
--- src/components/ui/header/backHeader/BackHeader.tsx
+++ src/components/ui/header/backHeader/BackHeader.tsx
@@ -7,7 +7,7 @@ interface BackHeaderProps {
 
 const BackHeader: React.FC<BackHeaderProps> = ({ title, onBack }) => {
     return (
-        <div className="w-[390px] h-[90px] px-[16px] py-[8px] shadow-top flex items-end justify-center shadow-header">
+        <div className="w-[385px] h-[90px] px-[16px] py-[8px] bg-neutral-white shadow-top flex items-end justify-center shadow-header">
             <div className="relative flex items-center w-full h-[48px]">
                 <div className="absolute left-0 flex items-center justify-start cursor-pointer w-[30px] h-[30px]" onClick={onBack}>
                     <BackArrow />
